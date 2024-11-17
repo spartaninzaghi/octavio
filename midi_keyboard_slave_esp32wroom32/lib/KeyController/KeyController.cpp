2c24fec66f92c056c4e801acfa72543839440c22
Mate Narh <spartaninzaghi@gmail.com>
2024-11-17 03:16:11 -0500

    ADC on esp32 very noisy. more ADC observation

--- midi_keyboard_slave_esp32wroom32/lib/KeyController/KeyController.cpp
+++ midi_keyboard_slave_esp32wroom32/lib/KeyController/KeyController.cpp
@@ -117,11 +117,16 @@ void KeyController::run()
         //
         // Debug lines
         //
-        Serial.print(" | Key: "); Serial.print(key->GetNote());
-        Serial.print(" | Readiness: "); Serial.print(key->IsReadyForMIDI());
-        Serial.print(" | Velocity: "); Serial.print(key->GetVelocity());
-        Serial.println(" | Status: "); Serial.print(key->GetStatus());
+        if (key->IsReadyForMIDI())
+        {
+            Serial.print(" | Key: ");       Serial.print(key->GetNote());
+            Serial.print(" | Readiness: "); Serial.print(key->IsReadyForMIDI());
+            Serial.print(" | Velocity: ");  Serial.print(key->GetVelocity());
+            Serial.print(" | Status: ");    Serial.print(key->GetStatus());
+            Serial.println();
+        }
     }
+    delay(80);
 
     //
     // Send the packet containing the data on the keys of this controller to the master
