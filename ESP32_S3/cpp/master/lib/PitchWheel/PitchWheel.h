b16f516b2b994823a5c26ef34d1afcb786142b4e
Mate Narh <spartaninzaghi@gmail.com>
2024-11-21 14:27:59 -0500

    type changed from int to int16_t to trigger overload function for pitch bend.

--- ESP32_S3/cpp/master/lib/PitchWheel/PitchWheel.h
+++ ESP32_S3/cpp/master/lib/PitchWheel/PitchWheel.h
@@ -9,52 +9,54 @@
 #ifndef PITCH_WHEEL_H
 #define PITCH_WHEEL_H
 
+#include <Arduino.h>
+
 class PitchWheel
 {
     private:
 
         /// The GPIO pin used to poll analog data for this Picth Wheel
-        const int mPin = 0; 
+        const int mPitchBendPin = 4; 
 
         /// The bend value for this Pitch Wheel. Ideally [-8192 <-> 0 <-> 8192]
-        int mBend = 0;    
+        int16_t mBend = 0;  
 
         /// Accounts for deadzone region at center of analog joystick module
-        int mHysteresis = 10;  
+        int16_t mHysteresis = 50;  
         
-        int mMinBend = 0;      ///< The minimum pitch bend value scaled from the analog sensor of this pitch wheel
-        int mMaxBend = 0;      ///< The maximum pitch bend value scaled from the analog sensor of this pitch wheel
+        int16_t mMinBend = 0;      ///< The minimum pitch bend value scaled from the analog sensor of this pitch wheel
+        int16_t mMaxBend = 0;      ///< The maximum pitch bend value scaled from the analog sensor of this pitch wheel
 
-        int mDeadzoneMin = 0;  ///< The lower boundary of the deadzone. Values within the deadzone are treated as 0
-        int mDeadzoneMax = 0;  ///< The upper boundary of the deadzone. Values within the deadzone are treated as 0
+        int16_t mDeadzoneMin = 1800;  ///< The lower boundary of the deadzone. Values within the deadzone are treated as 0
+        int16_t mDeadzoneMax = 2050;  ///< The upper boundary of the deadzone. Values within the deadzone are treated as 0
 
+        int mSampleSize = 100;      ///< Number of ADC values to sample for sensor callibration
 
     public:
 
-        PitchWheel(const int minBend, const int maxBend);
+        PitchWheel(const int pitchBendPin, const int16_t minBend, const int16_t maxBend);
         ~PitchWheel();
         
         // ----------------------------------- Setters ------------------------------------
-        int GetBend() const;
-        int GetMinBend() const;
-        int GetMaxBend() const;
-        int GetDeadzoneMin() const;
-        int GetDeadzoneMax() const;
+        int16_t GetBend() const;
+        int16_t GetMinBend() const;
+        int16_t GetMaxBend() const;
+        int16_t GetDeadzoneMin() const;
+        int16_t GetDeadzoneMax() const;
 
         // ----------------------------------- Getters ------------------------------------
-        void SetMinBend(const int bendMin);
-        void SetMaxBend(const int bendMax);
-        void SetDeadzoneMin(const int deadzoneMin);
-        void SetDeadzoneMax(const int deadzoneMax);
+        void SetMinBend(const int16_t bendMin);
+        void SetMaxBend(const int16_t bendMax);
+        void SetDeadzoneMin(const int16_t deadzoneMin);
+        void SetDeadzoneMax(const int16_t deadzoneMax);
         
         // --------------------------------- Core Methods ---------------------------------
         void Update();
 
 
         PitchWheel() = delete;                       ///< Default constructor disabled
-        PitchWheel(const PitchWheel &) = delete;  ///< Copy constructor disabled
+        PitchWheel(const PitchWheel &) = delete;     ///< Copy constructor disabled
         void operator=(const PitchWheel &) = delete; ///< Assignment operator disabled
 };
 
 #endif // PITCH_WHEEL_H
-
