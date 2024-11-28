72c310ab9da92a7d959ca39cfe8caec2d0af2a21
Mate Narh <spartaninzaghi@gmail.com>
2024-11-28 02:01:41 -0500

    improved documentation for A3144 test

--- tests/Arduino/A3144DigitalHallEffectSensorTest/A3144DigitalHallEffectSensorTest.ino
+++ tests/Arduino/A3144DigitalHallEffectSensorTest/A3144DigitalHallEffectSensorTest.ino
@@ -1,35 +1,58 @@
 /*
- * @file RotaryEncoderAndJoyStickTest.ino
+ * @file A3144DigitalHallEffectSensorTest.ino
  * @author Mate Narh
  *
- * This Arduino sketch is used to test a  joystick module using an ADC pin
- * on the ESP32 S3. The joystick module's x-axis is used for MIDI pitch bending
+ * This Arduino sketch is used to test an A3144 digital hall effect sensor on 
+ * the ESP32 S3.
  *
- * After successful iterations of this test, the underlying logic will be integrated
- * into the Octavio master driver.
+ * Hardware Setup
+ * --------------
+ *
+ * - Each of the 3 A3144 pins (SUPPLY, GROUND, DIGITAL OUTPUT) is connected 
+ *   along correspondingroute, such that:
+ *
+ *   -- SUPPLY -> 5V source [Please ensure that a 5V supply is used since the operating
+ *                       voltage of the A3144 module is 4.5V - 24V. I forgot earlier
+ *                       and used the 3.3V out of the MCU, which, as expected LED to
+ *                       inconsistent results. So please cross-check]
+ *                       datasheet: https://www.mpja.com/download/a3144eul.pdf
+ * 
+ *   -- GROUND -> MCU [ESP32 S3] GND
+ *
+ *   -- OUTPUT -> Digital GPIO Pin for receiving data from A3144. This pin should be
+ *                configured as an INPUT pin, with wiring that ties a pullup resistor
+ *                (eg. 10k-ohms) between the output and GND to prevent the output from
+ *                floating. Please ensure this to avoid indeterminism. The A3144 uses
+ *                ACTIVE LOW logic. So when its output is LOW, a magnet is present ...  
+ *
+ * Additional Note:
+ * If using multiple A3144's please ensure that your power supply is able to drive both
  *
  * This test is conducted on the Arduino IDE 2.3.3, and the MCU used is
- * the ESP32-S3-DevkitC1-N16R8  
+ * the ESP32-S3-DevkitC1-N16R8V  
  */
 
-#define HES_1 17
-#define HES_2 18
+#define HALL_EFFECT_SENSOR_1 17
+#define HALL_EFFECT_SENSOR_2 18
 
 int value1 = 0;
 int value2 = 0;
 
 void setup() {
+
   Serial.begin(115200);
-  pinMode(HES_1, INPUT_PULLUP);
-  pinMode(HES_2, INPUT_PULLUP);
+
+  pinMode(HALL_EFFECT_SENSOR_1, INPUT_PULLUP);
+  pinMode(HALL_EFFECT_SENSOR_2, INPUT_PULLUP);
 
 }
 
 void loop() {
 
-  value1 = digitalRead(HES_1);
+  value1 = digitalRead(HALL_EFFECT_SENSOR_1);
   delay(1);
-  value2 = digitalRead(HES_2);
+
+  value2 = digitalRead(HALL_EFFECT_SENSOR_2);
   delay(1);
 
   Serial.println(value1);
