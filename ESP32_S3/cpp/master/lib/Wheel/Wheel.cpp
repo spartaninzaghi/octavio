0a600625c3a0a88a19e04935b4687f45d7a32241
Mate Narh <spartaninzaghi@gmail.com>
2024-11-30 00:17:42 -0500

    Pitch wheel class generalized to wheel class, allowing the creation of both a pitch wheels, modulation wheels, etc. via polymorphism

---
+++ ESP32_S3/cpp/master/lib/Wheel/Wheel.cpp
@@ -0,0 +1,191 @@
+#include "Wheel.h"
+/**
+ * @file Wheel.cpp
+ * @author Mate Narh
+ */
+
+
+/**
+ * @brief Constructor
+ * @param mWheelPin The ADC pin for reading this wheel's analog values
+ * @param rangeMin The minimum value readable by the analog sensor for this pitch wheel
+ * @param rangeMax The maximum value readable by the analog sensor for this pitch wheel
+ */
+Wheel::Wheel(const int wheelPin, const int resolution, const int16_t rangeMin, const int16_t rangeMax) : 
+    mWheelPin(wheelPin), mRangeMin(rangeMin), mRangeMax(rangeMax)
+{
+    //
+    // Set the maximum analog value at which this Wheel's ADC pin(s) will be analog read
+    // Bit math for: MAX ADC VALUE = 2^resolution - 1 
+    //
+    mMaxAnalogValue = (1 << resolution) - 1;
+
+    //
+    // Callibrate this wheel
+    //
+    Callibrate();
+    
+    //
+    // Initialize digital EMA filter to smoothen out ADC noise
+    //
+    mDigitalFilter = new DigitalFilter(mSmoothingFactor);
+}
+
+/**
+ * @brief Destructor
+ */
+Wheel::~Wheel()
+{
+
+}
+
+/**
+ * @brief Get the current bend value of this pitch wheel
+ */
+int16_t Wheel::GetReading() const
+{
+    return mReading;
+}
+
+/**
+ * @brief Get the minimum bend readable by this pitch wheel's analog sensors
+ * @return The minimum bend readable by this pitch wheel's analog sensors
+ */
+int16_t Wheel::GetRangeMin() const
+{
+    return mRangeMin;
+}
+
+/**
+ * @brief Get the maximum bend readable by this pitch wheel's analog sensors
+ * @return The maximum bend readable by this pitch wheel's analog sensors
+ */
+int16_t Wheel::GetRangeMax() const
+{
+    return mRangeMax;
+}
+
+/**
+ * @brief Get the deadzone floor for this pitch wheel
+ * @return The deadzone floor for this pitch wheel
+ */
+int16_t Wheel::GetDeadzoneMin() const
+{
+    return mDeadzoneMin;
+}
+
+/**
+ * @brief Get the deadzone ceiling for this pitch wheel
+ * @return The deadzone ceiling for this pitch wheel
+ */
+int16_t Wheel::GetDeadzoneMax() const
+{
+    return mDeadzoneMax;
+}
+
+/**
+ * @brief Returns whether or not the bend value for this pitch wheel changed
+ * @return Flag indicating whether or not the bend value for this pitch wheel changed
+ */
+bool Wheel::IsReadingChanged() const
+{
+    return mReadingChanged;
+}
+
+/**
+ * @brief Set the minimum bend value read by this pitch wheel's analog sensors
+ * @param bendMin The new minimum bend value to set
+ */
+void Wheel::SetRangeMin(const int16_t bendMin)
+{
+    mRangeMin = bendMin;
+}
+
+/**
+ * @brief Se the maximum bend value readable by this pitch wheel's analog sensors
+ * @param bendMax The new bend deazone floor to set
+ */
+void Wheel::SetRangeMax(const int16_t bendMax)
+{
+    mRangeMax = bendMax;
+}
+
+/**
+ * @brief Set the minimum threshold for the deadzone
+ * @param deadzoneMin The new deadzone floor to set
+ */
+void Wheel::SetDeadzoneMin(const int16_t deadzoneMin)
+{
+    mDeadzoneMin = deadzoneMin;
+}
+
+/**
+ * @brief Set the maximum threshold for the deadzone
+ * @param deadzoneMax Thhe new deadzone ceiling to set
+ */
+void Wheel::SetDeadzoneMax(const int16_t deadzoneMax)
+{
+    mDeadzoneMax = deadzoneMax;
+}
+
+/**
+ * @brief Update the pitch bend value for this pitch wheel
+ */
+void Wheel::Update()
+{
+    int16_t value = mDigitalFilter->analogReadSmoothedWithEMA(mWheelPin);
+
+    mReading = (value < mDeadzoneMin) ? map(value, 0, mDeadzoneMin, mRangeMin, 0) :
+               (value > mDeadzoneMax) ? map(value, mDeadzoneMax, 4095, 0, mRangeMax) : 0; 
+
+    //
+    // Set the mReadingChanged flag if the value read for this wheel changed
+    //
+    if (mPrevReading != mReading)
+    {
+        mReadingChanged = true;
+        mPrevReading = mReading;
+    }
+    //
+    // Clear the mReadingChanged flag otherwise (the current readinge is the same as the last)
+    //
+    else
+    {
+        mReadingChanged = false;
+    }
+}
+
+/**
+ * @brief Callibrate this wheel on startup to adapt to transience and noise
+ */
+void Wheel::Callibrate()
+{
+    //
+    // Average 100 samples to callibrate the analog joystick module and
+    // set the minimum and maximum bend values
+    //
+    int sample = 0;
+    int sampleTotal = 0;
+    int16_t averageReading = 0;
+
+    for (int i = 0; i < mSampleSize; i++)
+    {
+        sample = analogRead(mWheelPin);
+        Serial.println(sample);
+
+        sampleTotal += sample;
+    }
+
+    averageReading = sampleTotal / mSampleSize;
+
+    mDeadzoneMin = averageReading - mHysteresis;
+    mDeadzoneMax = averageReading + mHysteresis;
+
+    Serial.println("Callibration Summary");
+    Serial.println("---------------------");
+    Serial.print("Wheel Pin: "); Serial.print(mWheelPin);
+    Serial.print(" | Average Reading: "); Serial.print(averageReading);
+    Serial.print(" | Deadzone Min: "); Serial.print(mDeadzoneMin);
+    Serial.print(" | Deadzone Max: "); Serial.print(mDeadzoneMax);
+    Serial.println(); 
+}
---
+++ ESP32_S3/cpp/master/lib/Wheel/Wheel.h
@@ -0,0 +1,76 @@
+/**
+ * @file Wheel.h
+ * @author Mate Narh
+ * 
+ * Class to interface with a joystick module or like sensor to drive MIDI 
+ * Pitch Bend messages in application
+ */
+
+#ifndef PITCH_WHEEL_H
+#define PITCH_WHEEL_H
+
+#include <Arduino.h>
+#include <DigitalFilter.h>
+
+
+class Wheel
+{
+    private:
+
+        const int mWheelPin = 4;      /// The ADC pin used to poll analog data for this Wheel
+        int mMaxAnalogValue = 1023;   /// The maximum analog value readable on the ADC pin(s) of this wheel
+
+        int16_t mReading = 0;         ///< The analog reading on the ADC pin of this wheel
+        int16_t mPrevReading = 0;     ///< The last analog value read on the ADC pin of this wheel
+        
+        int mSampleSize = 100;        ///< Number of ADC values to sample for sensor callibration
+        
+        int16_t mRangeMin = 0;        ///< The minimum pitch bend value scaled from the analog sensor of this pitch wheel
+        int16_t mRangeMax = 0;        ///< The maximum pitch bend value scaled from the analog sensor of this pitch wheel
+        
+        int16_t mDeadzoneMin = 1800;  ///< The lower boundary of the deadzone. Values within the deadzone are treated as 0
+        int16_t mDeadzoneMax = 2100;  ///< The upper boundary of the deadzone. Values within the deadzone are treated as 0
+
+        /// Add and subtract hysteresis to average reading during callibration to account for deadzone at the joystick's center
+        int16_t mHysteresis = 100;  
+
+        /// Smoothing factor to digitally filter analog data using Exponential Moving Average (EMA) LPF
+        const float mSmoothingFactor = 0.1;
+
+        /// The digital EMA filter for smoothening out high frequency noise on the ADC pin of this wheel
+        DigitalFilter* mDigitalFilter = nullptr;
+
+        /// Flag indicating whether the bend value for this pitch wheel changed
+        bool mReadingChanged = false;
+
+
+    public:
+
+        Wheel(const int wheelPin, const int resolution, const int16_t rangeMin, const int16_t rangeMax);
+        ~Wheel();
+        
+        // ----------------------------------- Setters ------------------------------------
+        int16_t GetReading() const;
+        int16_t GetRangeMin() const;
+        int16_t GetRangeMax() const;
+        int16_t GetDeadzoneMin() const;
+        int16_t GetDeadzoneMax() const;
+        bool IsReadingChanged() const;
+
+        // ----------------------------------- Getters ------------------------------------
+        void SetRangeMin(const int16_t bendMin);
+        void SetRangeMax(const int16_t bendMax);
+        void SetDeadzoneMin(const int16_t deadzoneMin);
+        void SetDeadzoneMax(const int16_t deadzoneMax);
+        
+        // --------------------------------- Core Methods ---------------------------------
+        void Update();
+        void Callibrate();
+
+
+        Wheel() = delete;                       ///< Default constructor disabled
+        Wheel(const Wheel &) = delete;          ///< Copy constructor disabled
+        void operator=(const Wheel &) = delete; ///< Assignment operator disabled
+};
+
+#endif // PITCH_WHEEL_H
